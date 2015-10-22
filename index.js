3a13fd0d134a105b4bf54229367bb7c8f175e233
asakusuma <asa.kusuma@gmail.com>
2015-10-22 15:03:49 -0700

    Add custom file to build

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 /* jshint node: true */
 'use strict';
 
+var fs = require('fs');
+var Plugin = require('broccoli-plugin');
+var mergeTrees = require('broccoli-merge-trees');
+var path = require('path');
+
+var customTestFilePath = 'my-custom-test-files/foo.js';
+
+AddCustomTestFile.prototype = Object.create(Plugin.prototype);
+AddCustomTestFile.prototype.constructor = AddCustomTestFile;
+function AddCustomTestFile(inputNodes) {
+  Plugin.call(this, inputNodes);
+}
+
+AddCustomTestFile.prototype.build = function() {
+  var outPath = path.join(this.outputPath, customTestFilePath);
+  fs.mkdirSync(path.dirname(outPath));
+  fs.writeFileSync(outPath, 'window.testing123 = true;');
+};
+
 module.exports = {
-  name: 'addon-test-bug'
+  name: 'addon-test-bug',
+  included: function(app) {
+    app.import({
+      test: customTestFilePath
+    });
+  },
+  postprocessTree: function(type, tree){
+    if (type === 'test') {
+      return mergeTrees([tree, new AddCustomTestFile([tree])]);
+    }
+    return tree;
+  }
 };
